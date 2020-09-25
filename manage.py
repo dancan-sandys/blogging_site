7525511e1a416516fcf6f3d4b80f071a938b4fae
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-25 20:33:34 +0300

    add a shell command to the application

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from .app import create_app
 from flask_script import Manager,Server
-
+from flask_migrate import Migrate,MigrateCommand
 
 
 
 app = create_app('development')
 
-manager = Manager()
+manager = Manager(app)
+migrate = Migrate(app,db)
 
 manager.add_command('server', Server)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app= app, db =db)
+
