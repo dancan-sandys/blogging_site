1543a4072f2fcf8eb882a34b2e1afb50a9226620
dancan-sandys <dancan.oruko96@gmail.com>
2020-09-28 21:08:09 +0300

    add styling to the forms used in the application

--- app/request.py
+++ app/request.py
@@ -5,14 +5,21 @@ quotes_url = 'http://quotes.stormconsultancy.co.uk/random.json'
 
 def load_quote():
 
-    with urllib.request.urlopen(quotes_url) as url:
-        response = url.read()
-        current_quote = json.loads(response)
+    try:
+        with urllib.request.urlopen(quotes_url) as url:
+            response = url.read()
+            current_quote = json.loads(response)
 
-        author = current_quote.get('author')
-        quote = current_quote.get('quote')
+            author = current_quote.get('author')
+            quote = current_quote.get('quote')
 
-        quote_displayed = Quotes(author,quote)
+            quote_displayed = Quotes(author,quote)
+    except:
+            author = 'Dancan'
+            quote = 'For Every Problem There is A solution'
+
+            quote_displayed = Quotes(author,quote)
+
 
     return quote_displayed
 
